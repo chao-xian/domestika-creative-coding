02300989817d3e67860f0d8c21175a22347ee865
chao-xian <dev@kelvingan.co.uk>
2025-07-05 18:26:43 +0100

    Add start of ex 6 and completed 5
    Now using Tweakpane which is cool.

--- creative-coding/sketches/sketch-04.js
+++ creative-coding/sketches/sketch-04.js
@@ -1,10 +1,23 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 const math = require('canvas-sketch-util/math');
+const Tweakpane = require('tweakpane');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
   animate: true
+};
+
+const params = {
+  cols: 10,
+  rows: 10,
+  scaleMin: 1,
+  scaleMax: 30,
+  freq: 0.001,
+  amp: 0.2,
+  frame: 0,
+  animate: true,
+  lineCap: 'butt',
 };
 
 const sketch = () => {
@@ -12,8 +25,8 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const cols = 10;
-    const rows = 10;
+    const cols = params.cols;
+    const rows = params.rows;
     const numCells = cols * rows;
 
     const gridW = width * 0.8;
@@ -34,12 +47,14 @@ const sketch = () => {
       const w = cellW * 0.8;
       const h = cellH * 0.8;
 
-      const n = random.noise2D(x + frame * 20, y, 0.001);
-      const angle = n * Math.PI * 0.2;
+      // const n = random.noise2D(x + frame * 20, y, params.freq);
+      const f = params.animate ? frame : params.frame;
+      const n = random.noise3D(x, y, f * 10, params.freq);
+      const angle = n * Math.PI * params.amp;
 
       // const scale = (n + 1) /2 * 30;
       // const scale = (n * 0.5 + 0.5) * 30;
-      const scale = math.mapRange(n, -1, 1, 5, 30);
+      const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
 
       context.save();
       context.translate(x,y);
@@ -48,6 +63,7 @@ const sketch = () => {
       context.rotate(angle);
 
       context.lineWidth = scale;
+      context.lineCap = params.lineCap;
 
       context.beginPath();
       context.moveTo(w * -0.5, 0);
@@ -58,4 +74,25 @@ const sketch = () => {
   };
 };
 
+const createPane = () => {
+  const pane = new Tweakpane.Pane();
+  pane.title = 'Grid noise';
+
+  let folder;
+
+  folder = pane.addFolder({title: 'Grid'});
+  folder.addInput(params, 'lineCap', { options: {butt: 'butt', round: 'round', square: 'square' }});
+  folder.addInput(params, 'cols', { min: 2, max: 50, step: 1});
+  folder.addInput(params, 'rows', { min: 2, max: 50, step: 1});
+  folder.addInput(params, 'scaleMin', { min: 1, max: 100 });
+  folder.addInput(params, 'scaleMax', { min: 1, max: 100 });
+
+  folder = pane.addFolder({title: 'Noise'});
+  folder.addInput(params, 'freq', { min: 0.001, max: 0.1 });
+  folder.addInput(params, 'amp', { min: 0, max: 1 });
+  folder.addInput(params, 'animate');
+  folder.addInput(params, 'frame', { min: 0, max: 999});
+}
+
+createPane();
 canvasSketch(sketch, settings);
